7fe2d3da3e28882554e595991e58f8200b57ba36
Paulikas <37594823+Paulikas@users.noreply.github.com>
2019-09-29 22:41:07 +0300

    Initializing project

---
+++ Pirma dalis/Strategy/Flight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirma_dalis.Strategy
+{
+    class Flight
+    {
+    }
+}
---
+++ Pirma dalis/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pirma_dalis
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
